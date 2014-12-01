733bf8b5bda8e7507c13030acf5076f1f3c6e423
jtufo <joe.tufo@meyouhealth.com>
2014-11-30 22:16:13 -0500

    Add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
   devise_for :admin_users
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
